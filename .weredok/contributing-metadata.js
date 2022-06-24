269e222734050a9bd90f96eb8cc4eb42ed88b58d
Weredok <93865249+Weredok@users.noreply.github.com>
2022-06-24 23:32:58 +0300

    Create contributing-metadata.js
    Signed-off-by: Weredok <93865249+Weredok@users.noreply.github.com>

--- .weredok/contributing-metadata.js
+++ .weredok/contributing-metadata.js
@@ -1,7 +1,6 @@
 module.exports = [{
   name: "metadata",
-  code: ` 
-  $title[1;$splittext[1]$textSplit[$splitText[2]$textSplit[$httpRequest[https://raw.githubusercontent.com/Weredok/docs/dev/functions/usd$tolowercase[$replacetext[$message[1];$;]];#];
+  code: `$title[1;$splittext[1]$textSplit[$splitText[2]$textSplit[$httpRequest[https://raw.githubusercontent.com/Weredok/docs/dev/functions/usd$tolowercase[$replacetext[$message[1];$;]];#];
   ];www.docs.weredok.ga/functions/usd$tolowercase[$replacetext[$message[1];$;]];#];
   ]]
   $addField[1;Описание;$splitText[2]$textSplit[$httpRequest[https://raw.githubusercontent.com/Weredok/docs/dev/functions/usd$tolowercase[$replacetext[$message[1];$;]];#];
@@ -10,6 +9,5 @@ module.exports = [{
   $addField[1;Пример использования;$splittext[1]$textSplit[$splitText[2]$textSplit[$httpRequest[https://raw.githubusercontent.com/Weredok/docs/dev/functions/usd$tolowercase[$replacetext[$message[1];$;]];## Пример использования];
   ]
   $color[1;RANDOM]  
-  $onlyif[$hasPerms[$guildID;$authorID;admin]==true;]
-  `
-}, {
+  $onlyif[$hasPerms[$guildID;$authorID;admin]==true;]`
+}]
